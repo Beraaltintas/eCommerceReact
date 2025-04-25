fa75eb0fdfbfefa6c7e6e33b4c9348330527c70c
Mustafa Bera Altıntaş <mustafaberaaltintas@gmail.com>
2025-04-25 14:24:40 +0300

    mongoDBconnection

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const port = 5000;
+dotenv.config();
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("connected mongo");
+  } catch (error) {
+    throw error;
+  }
+};
 const app = express();
 
-app.get("/", (req, res)=>{
-res.send("hello");
+app.get("/", (req, res) => {
+  res.send("hello");
 });
 
-app.get("/api", (req, res)=>{
-    res.send("apirouddte");
-})
+app.get("/api", (req, res) => {
+  res.send("apiroute");
+});
 
-app.listen(5000, ()=>{
-    console.log(`sunucu ${5000} portunda`);
-    
-})
+app.listen(port, () => {
+  connect();
+  console.log(`sunucu ${port} portunda`);
+});
