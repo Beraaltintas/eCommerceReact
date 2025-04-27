548afe28e44884ae83b15c004185c2868b38f93b
Mustafa Bera Altıntaş <mustafaberaaltintas@gmail.com>
2025-04-27 14:25:33 +0300

    singleCatogoryGet

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -1,30 +1,47 @@
 const express = require("express");
 const router = express.Router();
-const Category = require("../models/Category.js")
+const Category = require("../models/Category.js");
 
 //yeni kategori oluşturma (create)
-router.post("/", async(req, res)=>{
-    try {
-        const {name, img} = req.body;
-        const newCategory= new Category({name, img});
-        await newCategory.save();
-        res.status(201).json(newCategory);
-        
-    } catch (error) {
-        console.log(error);
-        
-    }
-})
+router.post("/", async (req, res) => {
+  try {
+    const { name, img } = req.body;
+    const newCategory = new Category({ name, img });
+    await newCategory.save();
+    res.status(201).json(newCategory);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // tüm ürünleri getirme
-router.get("/", async(req, res)=> {
+router.get("/", async (req, res) => {
+  try {
+    const categories = await Category.find();
+    res.status(200).json(categories);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "server error" });
+  }
+});
+
+//belirli bir kategori getirme(read-single)
+router.get("/:categoryId", async (req, res) => {
+  try {
+    const categoryId = req.params.categoryId;
+    
     try {
-        const categories = await Category.find();
-        res.status(200).json(categories);
+        const category = await Category.findById(categoryId);
+        res.status(200).json(category);
     } catch (error) {
         console.log(error);
-        res.status(500).json({error: "server error"});
         
+        res.status(404).json({error: "Category not found"});
     }
-})
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "server error" });
+  }
+});
 module.exports = router;
