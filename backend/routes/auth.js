87ea8d31125bf2d75fdd34264f703842f49933a5
Mustafa Bera Altıntaş <mustafaberaaltintas@gmail.com>
2025-04-28 18:38:46 +0300

    passwordHash

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs")
 const User = require("../models/User.js");
 
 //Kullanıcı oluşturma(Create-Register)
 router.post("/register", async (req, res) => {
   try {
     const { username, email, password } = req.body;
+    const existingUser = await User.findOne({email});
+    if(existingUser){
+        return res.status(400).json({error: "email adress is already registered."})
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = await new User({
       username,
       email,
-      password,
+      password: hashedPassword,
     });
     await newUser.save();
     res.status(201).json(newUser);
