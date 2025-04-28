446ca80db3e5273308b3761c92204699d59f642d
Mustafa Bera Altıntaş <mustafaberaaltintas@gmail.com>
2025-04-28 18:30:59 +0300

    userRegisterApi

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 //route dosyaları içe aktarma
 const productRoute = require("./products.js");
+const authRoute = require("./auth.js");
 const categoryRoute = require("./categories.js");
 //her rotayı ilgili yol altında kullanıyoruz
 router.use("/categories", categoryRoute);
+router.use("/auth", authRoute);
 router.use("/products", productRoute);
 
 module.exports = router;
