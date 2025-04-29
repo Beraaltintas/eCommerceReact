2f7c520ebb6c5342e9cf5ce8a13013bd6659726c
Mustafa Bera Altıntaş <mustafaberaaltintas@gmail.com>
2025-04-29 14:25:54 +0300

    createProduct

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const Product = require("../models/Product.js");
 
-// tüm ürünleri getirme
-router.get("/", async(req, res)=> {
-    res.send("ürünler geldi");
-})
+//yeni ürün oluşturma (create)
+router.post("/", async (req, res) => {
+    try {
+      const product = req.body;
+      const newProduct = new Product(product);
+      await newProduct.save();
+      res.status(201).json(newProduct);
+    } catch (error) {
+      console.log(error);
+    }
+  });
 module.exports = router;
