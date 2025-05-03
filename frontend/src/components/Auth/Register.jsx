3030fe1b7306afc04a43713da02e4d419e536f7a
Mustafa Bera Altıntaş <mustafaberaaltintas@gmail.com>
2025-05-03 13:11:13 +0300

    registerFormDataInput

--- frontend/src/components/Auth/Register.jsx
+++ frontend/src/components/Auth/Register.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    //formdan verileri alma
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  console.log(formData);
+
   return (
     <div className="account-column">
       <h2>Register</h2>
@@ -10,7 +23,7 @@ const Register = () => {
             <span>
               Username <span className="required">*</span>
             </span>
-            <input type="text" />
+            <input type="text" onChange={handleInputChange} name="username" />
           </label>
         </div>
         <div>
@@ -18,7 +31,7 @@ const Register = () => {
             <span>
               Email address <span className="required">*</span>
             </span>
-            <input type="email" />
+            <input type="email" onChange={handleInputChange} name="email" />
           </label>
         </div>
         <div>
@@ -26,7 +39,11 @@ const Register = () => {
             <span>
               Password <span className="required">*</span>
             </span>
-            <input type="password" />
+            <input
+              type="password"
+              onChange={handleInputChange}
+              name="password"
+            />
           </label>
         </div>
         <div className="privacy-policy-text remember">
