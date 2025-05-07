06112f8e71dfe576657633e3563a01cef634ee72
Mustafa Bera Altıntaş <mustafaberaaltintas@gmail.com>
2025-05-07 20:07:24 +0300

    productSeaerchApi

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -4,36 +4,35 @@ const Product = require("../models/Product.js");
 
 //yeni ürün oluşturma (create)
 router.post("/", async (req, res) => {
-    try {
-      const product = req.body;
-      const newProduct = new Product(product);
-      await newProduct.save();
-      res.status(201).json(newProduct);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: "server error" });
-    }
-  });
-  // tüm ürünleri getirme
-  router.get("/", async (req, res) => {
-    try {
-      const products = await Product.find();
-      res.status(200).json(products);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: "server error" });
-    }
-  });
-  //belirli bir ürün getirme(read-single)
+  try {
+    const product = req.body;
+    const newProduct = new Product(product);
+    await newProduct.save();
+    res.status(201).json(newProduct);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "server error" });
+  }
+});
+// tüm ürünleri getirme
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "server error" });
+  }
+});
+//belirli bir ürün getirme(read-single)
 router.get("/:productId", async (req, res) => {
   try {
     const productId = req.params.productId;
     const product = await Product.findById(productId);
-    if(!product){
+    if (!product) {
       return res.status(404).json({ error: "Product not found" });
     }
     res.status(200).json(product);
-
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "server error" });
@@ -52,11 +51,9 @@ router.put("/:productId", async (req, res) => {
       return res.status(404).json({ error: "product not found" });
     }
 
-    const updatedProduct = await Product.findByIdAndUpdate(
-      productId,
-      updates,
-      { new: true }
-    );
+    const updatedProduct = await Product.findByIdAndUpdate(productId, updates, {
+      new: true,
+    });
     res.status(200).json(updatedProduct);
   } catch (error) {
     console.log(error);
@@ -69,8 +66,8 @@ router.delete("/:productId", async (req, res) => {
   try {
     const productId = req.params.productId;
     const deletedProduct = await Product.findByIdAndDelete(productId);
-    if(!deletedProduct){
-        return res.status(404).json({error: "product not found"});
+    if (!deletedProduct) {
+      return res.status(404).json({ error: "product not found" });
     }
     res.status(200).json(deletedProduct);
   } catch (error) {
@@ -78,5 +75,18 @@ router.delete("/:productId", async (req, res) => {
     res.status(500).json({ error: "server error" });
   }
 });
-  
+// Product Search(name)
+router.get("/search/:productName", async (req, res) => {
+  try {
+    const productName = req.params.productName;
+
+    const products = await Product.find({
+      name: { $regex: productName, $options: "i" }, //regex her bir karakterde arar options i büyük küçük harf duyarsız
+    });
+    res.status(200).json(products);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "server error" });
+  }
+});
 module.exports = router;
